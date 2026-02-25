75d71c75e5bd25456714c2a16d249ed84023ba0d
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2026-02-24 19:44:50 -0600

    Add project files.

---
+++ WEB_UI/Controllers/PatientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class PatientsController : Controller
+    {
+        public IActionResult PatientList()
+        {
+            return View();
+        }
+    }
+}
