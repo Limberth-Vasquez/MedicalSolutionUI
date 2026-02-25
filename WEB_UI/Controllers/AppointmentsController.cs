bc9b1ff3c25f651e71a66ea4ac5a54c80d6a3bc7
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2026-02-24 21:26:47 -0600

    Se crean las vistas de listar pacientes, listar citas y crear citas a pacientes, pendiente finalizar JS

---
+++ WEB_UI/Controllers/AppointmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult AppointmentList()
+        {
+            return View();
+        }
+        public IActionResult CreateAppointment()
+        {
+            return View();
+        }
+    }
+}
