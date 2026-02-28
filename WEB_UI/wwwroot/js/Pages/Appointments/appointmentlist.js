ce023569bc3142af147bef0102f18aedda780e87
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2026-02-28 10:12:01 -0600

    se listan las citas por paciente usando ag grid

--- WEB_UI/wwwroot/js/Pages/Appointments/appointmentlist.js
+++ WEB_UI/wwwroot/js/Pages/Appointments/appointmentlist.js
@@ -1,1 +1,51 @@
-﻿
+﻿function AppointmentList() {
+
+    this.InitView = () => {
+        this.GetAppointments();
+        this.LoadContextInformation();
+
+        $('#btnCreate').click(() => {
+            var view = new AppointmentList();
+            view.RedirectCreatAppointment();
+        });
+    };
+
+    this.GetAppointments = () => {
+        $.ajax({
+            url: API_URL_BASE + "/api/Appointment/GetAppointmentByPatientId?patientId=" + sessionStorage["patientId"],
+            method: "GET",
+            dataType: "json",
+            contentType: "application/json;chartset=utf-8"
+        }).done((response) => {
+            if (response.result == "ok") {
+                console.log("Estos fueron los datos que recibimos del API", response.data);
+                gridOptions.api.setGridOption('rowData', response.data);
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Hubo un problema al cargar cita de paciente',
+                    text: 'Hubo un problema al cargar cita de paciente ' + response.message
+                });
+            }
+        }).fail((error) => {
+            console.error("Error del ajax", error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al cargar cita',
+                text: 'Hubo un error, contacte al administrador '
+            });
+        });
+    };
+
+    this.LoadContextInformation = () => {
+        $('#txtPacientData').val(sessionStorage["PatientData"]);
+    };
+
+    this.RedirectCreatAppointment = () => {
+        window.location = "/Appointments/CreateAppointment";
+    };
+}
+$(document).ready(() => {
+    let view = new AppointmentList();
+    view.InitView();
+});
--- WEB_UI/wwwroot/js/Pages/Appointments/griddefinition.js
+++ WEB_UI/wwwroot/js/Pages/Appointments/griddefinition.js
@@ -1,1 +1,24 @@
-﻿
+﻿const columnDefinitions = [
+    { field: "appointmentDate", headerName: "Fecha" },
+    { field: "title", headerName: "Titulo" },
+    { field: "speciality", headerName: "Especialidad" }
+];
+
+const gridOptions = {
+    columnDefs: columnDefinitions,
+    rowData: [],
+    rowSelection: 'single',
+
+    // defaults
+    defaultColDef: { sortable: true, filter: true },
+
+    //Events
+    onGridReady: (params) => {
+        gridOptions.api = params.api;
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDiv = document.querySelector('#myGridAppointments');
+    agGrid.createGrid(gridDiv, gridOptions);
+});
